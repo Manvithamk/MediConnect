bc88d476a824b7df4bbf6ef6f77cb4547bbf4811
Manvithamk <dummy@gmail.com>
2026-01-21 19:08:24 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/repository/PatientRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/PatientRepository.java
@@ -1,12 +1,12 @@
 package com.edutech.progressive.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Patient;
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer>{
 
-public interface PatientRepository extends JpaRepository<Patient,Integer> {
-
-    List<Patient> findAllOrderByNameAsc();
+    Patient findByPatientId(int patientId);
 }
